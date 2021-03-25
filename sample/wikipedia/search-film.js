e6b6366ec9cd9c1f97ba1d5ed1a52a711bcaeea1
Sean Chen <Sean.Chen@leocorn.com>
2021-03-25 10:14:39 -0400

    update to use Promise.all to run all query in parallel.

--- sample/wikipedia/search-film.js
+++ sample/wikipedia/search-film.js
@@ -18,9 +18,34 @@ if( rawParams.length < 1 ) {
     process.exit()
 }
 
-testSearch(rawParams[0]);
+// query in sequence.
+//testSearch(rawParams[0]);
+// query in parallel
+testSearchAll( rawParams[0] );
 
-async function testSearch(filmTitle) {
+/**
+ * this test function will do exactly the same work as the following
+ * function, except it is using Promise.all to run all query in parallel.
+ */
+async function testSearchAll( filmTitle ) {
+
+    const [poster, excerpt] = await Promise.all([
+        wiki.filmPoster( filmTitle ),
+        wiki.filmExcerpt( filmTitle )
+    ]);
+
+    console.log(`Poster: ${poster}`);
+    console.log("=======");
+    console.log(`Excerpt: ${excerpt[0]}`);
+    console.log("=======");
+    console.log(`Article: ${excerpt[1]}`);
+}
+
+/**
+ * quick test to get the poster, excerpt and article url for the
+ * given file title.
+ */
+async function testSearch( filmTitle ) {
 
     let poster = await wiki.filmPoster( filmTitle );
     let excerpt = await wiki.filmExcerpt( filmTitle );
